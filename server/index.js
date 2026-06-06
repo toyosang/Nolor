d3c650555993ba691bd22fc31f152102e90dca28
toyosang <zahramoutji@gmail.com>
2026-06-06 03:14:38 +0100

    Change server to use httpServer and listen on all interfaces

--- server/index.js
+++ server/index.js
@@ -159,6 +159,6 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
+httpServer.listen(port, '0.0.0.0', () => {
   console.log(`🚀 السيرفر يعمل على البورت ${PORT}`);
 });
